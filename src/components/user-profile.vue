4ce36c57a2a93076aba711e27edd110c835f3ffd
Robby Bourne <robby.bourne@ozk.com>
2019-09-13 08:35:41 -0400

    flex wrap added

--- src/components/user-profile.vue
+++ src/components/user-profile.vue
@@ -1,16 +1,16 @@
 <template>
-            <v-container class="col-12">
-                <div class="d-flex flex-column align-center justify-space-between">
-                    <v-card class="d-flex flex-column align-center col-2 justify-space-around" raised outlined v-for="user in users" :key="user.name"> 
-                        <v-card-title class="">       
-                                {{user.name}} 
-                        </v-card-title>
-                        <v-card-text class="d-flex justify-center">Height: {{user.height}} cms</v-card-text>
-                        <v-card-text class="d-flex justify-center">Weight: {{user.mass}} lbs</v-card-text>
-                        <v-card-text class="d-flex justify-center">DOB: {{user.created}}</v-card-text>
-                    </v-card>    
-                </div>
-            </v-container>  
+        <v-container class="col-10">
+            <div class="d-flex flex-row flex-wrap justify-center col-12">
+                <v-card class="d-flex flex-column align-center col-3 justify-space-around" raised outlined v-for="user in users" :key="user.name"> 
+                    <v-card-title class="">       
+                            {{user.name}} 
+                    </v-card-title>
+                    <v-card-text class="d-flex justify-center">Height: {{user.height}} CMs</v-card-text>
+                    <v-card-text class="d-flex justify-center">Weight: {{user.mass}} KGs</v-card-text>
+                    <v-card-text class="d-flex justify-center">DOB: {{user.birth_year}}</v-card-text>
+                </v-card>    
+            </div>
+        </v-container>  
 </template>
 
 <script>
